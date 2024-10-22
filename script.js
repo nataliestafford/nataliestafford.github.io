9218ade8940cc55de98290fe0980e6f71315399a
nataliestafford <nstaffo1@samford.edu>
2024-10-22 14:02:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function downloadData() {
+function filterData() {
   event.preventDefault();
   var startdate = document.getElementById("startdate").value;
   var enddate = document.getElementById("enddate").value;
